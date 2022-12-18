4b82260bc06453b356cbb4359dc667155f11cfe3
msa0044 <msa820258400044@gmail.com>
2022-12-18 17:31:30 -0300

    edição de atributo horas em entradaEntity

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -1,18 +1,68 @@
-window.onload = function () {
-}
-
-function getOne(cracha) {
+function getOneFuncionario(cracha) {
     $.get("http://localhost:8080/funcionario/get/" + cracha, function (resultado) {
-        console.log(resultado);
         $("#funcionario").val(resultado.nome);
         $("#setor").val(resultado.setor);
+    })
+}
+
+function toDate(milissegundos) {
+    dateFormat = new Date(milissegundos)
+    return zeroLeft(2,dateFormat.getDate()) +
+        "/" + zeroLeft(2,(dateFormat.getMonth() + 1)) +
+        "/" + dateFormat.getFullYear()
+}
+
+function toHour(milissegundos) {
+    dateFormat = new Date(milissegundos)
+    return zeroLeft(2, dateFormat.getHours()) +
+        ":" + zeroLeft(2, dateFormat.getMinutes())
+}
+
+function zeroLeft(tamanho, numero) {
+    var a = ""
+    if ((numero+"").length < tamanho) {
+        while (a.length + 1 < tamanho) {
+            a += "0"
+        }
+        return a += numero
+    } else {
+        return numero
+    }
+}
+
+function getAllEntradas(cracha) {
+    $.get("http://localhost:8080/entrada/get/all", function (resultado) {
+        resultado.forEach(function (entrada) {
+            let row = $("<tr>")
+            let col
+            let dateFormat
+
+            col += ("<td>" + entrada.id + "</td>")
+            col += ("<td>" + toDate(entrada.data) + "</td>")
+            col += ("<td>" + zeroLeft(8, entrada.funcionario.cracha) + "</td>")
+            col += ("<td>" + entrada.funcionario.nome + "</td>")
+            col += ("<td>" + entrada.funcionario.setor + "</td>")
+            col += ("<td>" + entrada.modalidade + "</td>")
+            col += ("<td>" + toHour(entrada.entradaPrevista) + "</td>")
+            col += ("<td>" + toHour(entrada.saidaPrevista) + "</td>")
+            col += ("<td>" + toHour(entrada.entradaValidada) + "</td>")
+            col += ("<td>" + toHour(entrada.saidaValidada) + "</td>")
+            col += ("<td>" + toHour(entrada.horas) + "</td>")
+            col += ("<td>" + entrada.observacao + "</td>")
+
+            col += ("<td>" + "<i class='fa fa-trash'></i>" + "</td>")
+            col += ("<td>" + "<i class='fa fa-pencil'></i>" + "</td>")
+
+            row.append(col)
+            $("#tab").append(row)
+        })
     })
 }
 
 function procurarFuncionario() {
     campo = $("#cracha")[0]
     if (campo.value.length == 8) {
-        getOne(campo.value)
+        getOneFuncionario(campo.value)
     }
 }
 
@@ -44,13 +94,16 @@ function criarObjeto() {
     };
     postFuncionario(funcionario)
     var entrada = {
+        "data": $("#data")[0].value,
         "funcionario": funcionario,
         "modalidade": $("#modalidade")[0].value,
-        "data": $("#data")[0].value,
         "entradaPrevista": $("#horaEntrada")[0].value,
         "saidaPrevista": $("#horaSaida")[0].value,
         "observacao": $("#observacao")[0].value
     }
     postEntrada(entrada)
-    console.log(entrada)
+}
+
+window.onload = function () {
+    getAllEntradas()
 }
